9f42e8ac5ce62f42ef74123e11474c22a3a4829b
tomfang@126.com <tomfang@126.com>
2017-11-29 19:31:07 +0800

    补提交

---
+++ ZhouJF.Management.WebUI/Scripts/ImgInfo/Add.js
@@ -0,0 +1,59 @@
+﻿function pageReady() {
+    $("#sure").click(function () {
+        var checkResult = true;
+        CommitForm();
+    });
+}
+
+function CommitForm() {
+    $.ajax({
+        cache: true,
+        type: "POST",
+        url: "/ImgInfo/Add",
+        data: $('#DataAdd').serialize(),
+        async: false,
+        error: function (request) {
+            bootbox.alert("error");
+        },
+        success: function (data) {
+            if (data.IsSuccess) {
+                // window.location.href = "/ImgInfo/Index";
+                alert("添加成功！");
+                location.reload();
+            } else {
+                if (data.ErrorCode == -1) {
+                    bootbox.alert("删除出错:TOKEN错误，请重新登录！");
+                } else {
+                    bootbox.alert(data.Message);
+                }
+            }
+        }
+    });
+}
+
+function del(el, id) {
+    bootbox.confirm("确定删除,删除以后将无法恢复该数据和相关联的数据?", function (result) {
+        if (result === true) {
+            $.ajax({
+                url: "/ImgInfo/Delete/" + id,
+                type: "POST",
+                dataType: "json",
+                success: function (data) {
+                    if (data.IsSuccess) {
+                        $('#grid').dataTable().fnDraw();
+                        location.reload();
+                    } else {
+                        if (data.ErrorCode == -1) {
+                            bootbox.alert("删除出错:TOKEN错误，请重新登录！");
+                        } else {
+                            bootbox.alert("删除出错:" + JSON.stringify(data.Message));
+                        }
+                    }
+                },
+                error: function (XMLHttpRequest, textStatus, errorThrown) {
+                    bootbox.alert("error:" + this.url);
+                }
+            });
+        }
+    });
+}
